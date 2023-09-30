5af98ca303c0fe9bca2e9f29f7845023a9369f28
Ali Chaabane <milik9898>
2023-09-30 12:48:43 +0100

    init project

---
+++ src/main/java/com/assocation/justice/JusticeApplication.java
@@ -0,0 +1,13 @@
+package com.assocation.justice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JusticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assocation/justice/JusticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assocation.justice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
