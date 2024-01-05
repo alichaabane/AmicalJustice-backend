1dc176315bacb543b4cb290c7a7e53361f524330
Ali Chaabane <milik9898>
2024-01-05 23:18:35 +0100

    fix docker compose conf v6

--- start-prod.sh
+++ start-prod.sh
@@ -1,3 +1,3 @@
-docker rmi justice-backend:v1
+docker rmi localhost:5000/justice-backend:1.0.0
 ENV_FILE=./.env.prod docker-compose up -d  --force-recreate
 #ENV_FILE=./.env.prod docker-compose up -d --build
