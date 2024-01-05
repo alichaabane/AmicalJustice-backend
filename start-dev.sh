b14f6761730e38c5d65cf52ded4c93e60bcfabff
Ali Chaabane <milik9898>
2024-01-06 00:20:52 +0100

    fix docker compose conf v9

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
-docker rmi localhost:5000/justice-backend:1.0.0
+#docker rmi localhost:5000/justice-backend:1.0.0
 ENV_FILE=./.env.dev docker-compose up -d  --force-recreate
 #ENV_FILE=./.env.dev docker-compose up -d --build
