642653272f42837ced683b01503b0dd7986ad642
Ali Chaabane <milik9898>
2024-01-05 20:36:02 +0100

    fi bash files v3

--- start-dev.sh
+++ start-dev.sh
@@ -1,2 +1,3 @@
 docker rmi justice-backend:v1
-ENV_FILE=./.env.dev docker-compose up -d --build
+ENV_FILE=./.env.dev docker-compose up -d  --force-recreate
+#ENV_FILE=./.env.dev docker-compose up -d --build
