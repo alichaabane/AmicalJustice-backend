485c42c57e57db08c2b1c290dc58dc7ce8072fdf
Ali Chaabane <milik9898>
2024-01-05 20:33:22 +0100

    fi bash files v2

--- start-prod.sh
+++ start-prod.sh
@@ -1,2 +1,3 @@
 docker rmi justice-backend:v1
-ENV_FILE=./.env.prod docker-compose up -d --build
+ENV_FILE=./.env.prod docker-compose up -d  --force-recreate
+#ENV_FILE=./.env.prod docker-compose up -d --build
